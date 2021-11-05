6cd10329b5cb72527014d9d44a910067671b9ed4
david0807 <phoenix080733@outlook.com>
2021-11-06 06:13:42 +0900

    test4 design

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
 /** This is for testing. */
-echo "to test git";
-echo "push from core";
-echo "[core test3]".phpinfo();
-echo "to test git push test3";
-echo "push from design test3";
-echo "[design test3]".phpinfo();
+echo "this is proc for design";
+function sumNumber($a, $b) {
+    echo $a + $b;
+}
 
 ?>
