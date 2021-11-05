95afc30d62f4d7cc293c24497f1ce8c1a98888b3
david0807 <phoenix080733@outlook.com>
2021-11-06 06:04:27 +0900

    test3 core

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 /** This is for testing. */
-echo "to test git";
-echo "push from core";
-echo "[core]".phpinfo();
-echo "to test git push test2";
-echo "push from design test2";
+echo "to test git test3";
+echo "push from core test3";
+echo "[core test3]".phpinfo();
+echo "to test git push test2 core_test3";
+echo "push from design test2 core_test3";
 echo "[design test2]".phpinfo();
 
 ?>
